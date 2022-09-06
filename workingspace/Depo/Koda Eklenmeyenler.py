98c9273778d4a10f8e6ae7fade521d234a72502c
eceaksen <90172644+eceaksen@users.noreply.github.com>
2022-09-06 21:07:27 +0300

    Delete workingspace directory

--- workingspace/Depo/Koda Eklenmeyenler.py
+++
@@ -1,7 +0,0 @@
-# ---#
-        st.subheader("Total Activity Duration - (Request Types-Sınıf_Tipi)")
-        piv = df1.pivot_table(index="İstek_Tipi_Detay", columns="Sınıf_Tipi", values="Toplam_Aktivite_Süresi")
-        st.write(piv)
-        st.subheader("Total Sla Duration - (Request Types-Sınıf_Tipi)")
-        piv2 = df1.pivot_table(index="İstek_Tipi_Detay", columns="Sınıf_Tipi", values="Sla_Süresi")
-        st.write(piv2)
